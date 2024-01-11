50c255d0e9e0509054583fe56d4ecc1fbdcafb09
Aman <aman01428@gmail.com>
2024-01-12 01:37:16 +0530

    Added auto logout

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -8,16 +8,19 @@ const AuthContext=React.createContext({
 })
 
 export const AuthContextProvider=(props)=>{
-    const [token,setToken]=useState(null);
+    const initialToken=localStorage.getItem("token");
+    const [token,setToken]=useState(initialToken);
  
     const useIsLoggedIn=!!token;
 
     const loginHandler=(token)=>{
         setToken(token)
+        localStorage.setItem("token",token);
     };
 
     const logouthandler=()=>{
         setToken(null);
+        localStorage.removeItem("token");
     };
 
     const contextValue={
